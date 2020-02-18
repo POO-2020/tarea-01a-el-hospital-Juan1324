63a19f21589df1ea40a4f2afca629a61235bf48b
Juan Ramos <jramos32@ucol.mx>
2020-02-18 01:45:03 -0600

    getNombreCompleto

--- nombre.js
+++ nombre.js
@@ -1,8 +1,20 @@
-class nombre {
+class Nombre {
     constructor(nombre,apellidoPaterno,apellidoMaterno){
         this.nombre=nombre;
-        this.apellidoPaternor=apellidoPaterno;
+        this.apellidoPaterno=apellidoPaterno;
         this.apellidoMaterno=apellidoMaterno;
 
     }
+    getNombreCompleto(){
+        return(`${this.nombre}${this.apellidoPaterno}${this.apellidoMaterno}`)
+
+    }
+    getApellidoNombre(){
+
+    }
+    getIniciales(){
+
+    }
 }
+let nombre1= new Nombre("Juan ","Ramos"," Campos");
+console.log(nombre1.getNombreCompleto())
