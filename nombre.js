f99fc644634bb78d646d0980867830e3753b7f81
Juan Ramos <jramos32@ucol.mx>
2020-02-18 01:36:21 -0600

    constructor

--- nombre.js
+++ nombre.js
@@ -1,1 +1,8 @@
+class nombre {
+    constructor(nombre,apellidoPaterno,apellidoMaterno){
+        this.nombre=nombre;
+        this.apellidoPaternor=apellidoPaterno;
+        this.apellidoMaterno=apellidoMaterno;
 
+    }
+}
