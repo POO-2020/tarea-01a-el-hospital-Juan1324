4f07f75bb6cbd03809c1b0eb4458654f7f73657d
Juan Ramos <jramos32@ucol.mx>
2020-02-19 12:22:45 -0600

    nuevo constructor

--- fecha.js
+++ fecha.js
@@ -1,13 +1,13 @@
 class Fecha {
-    constructor(){
+    constructor(fecha){
+        this.fecha= fecha;
+        this.fechaHoy= new Date();
+        this.años = Math.floor(((this.fechaHoy - this.fecha) / (1000 * 60 * 60 * 24) / 365));
  
 
 
     }
     getAños(){
-
-        {
-
-        }
     }
+    
 }
