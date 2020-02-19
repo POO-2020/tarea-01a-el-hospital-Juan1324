ca2c457b2565974a20e02d7e632a0700ab8883f5
Juan Ramos <jramos32@ucol.mx>
2020-02-19 10:10:24 -0600

    constructor

---
+++ tiempo.js
@@ -0,0 +1,12 @@
+class Tiempo{
+    constructor(minutos,hora,periodo){
+        this.hora=hora;
+        this.minutos=minutos;
+        this.periodo=periodo;
+
+        getFormato12(){
+            
+        }
+
+    }
+}
