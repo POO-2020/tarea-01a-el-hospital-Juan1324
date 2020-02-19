4f63204319445acca3097767919e8f861b4b79db
Juan Ramos <jramos32@ucol.mx>
2020-02-19 10:29:37 -0600

    getFormato24

--- tiempo.js
+++ tiempo.js
@@ -1,17 +1,22 @@
 class Tiempo{
-    constructor(minutos,hora,periodo){
+    constructor(hora,minutos,periodo){
         this.hora=hora;
         this.minutos=minutos;
         this.periodo=periodo;
+    }
+        
 
         getFormato12(){
             return `${this.hora}:${this.minutos} ${this.periodo}`
-    
         }
-        getFormato1224(){
-
+        getFormato24(){
+            if (this.hora !=12 && this.periodo=="pm"){
+                this.hora=this.hora+12;
+            }
+            return(`${this.hora}:${this.minutos}`)
 
         }
-
-    }
 }
+let hora= new Tiempo(1,55,"pm")
+console.log(hora.getFormato12())
+console.log(hora.getFormato24())
