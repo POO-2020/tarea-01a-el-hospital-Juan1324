4beb6d9f427045ac7e00cb8e6a2747c9199e49bf
Juan Ramos <jramos32@ucol.mx>
2020-02-22 23:12:41 -0600

    fin

--- hospital.js
+++ hospital.js
@@ -1,21 +1,29 @@
-class Hospital{
-    constructor(nombre,direcciones,doctor,citas){
+import Doctor from "./doctor.js"
+export default class Hospital{
+    constructor(nombre,direcciones){
         this.nombre=nombre;
         this.direcciones=direcciones;
         this.doctores=[]
         this.citas=[]
     }
-    registrarDoctores(doctor){
-        this.doctor.push(doctor)
+    registrarDoctor(doctor){
+        this.doctores.push(doctor)
 
     }
     listarDoctores(){
+        this.doctores.forEach((doctor, x) => {
+            console.log(`(${x+1}) ${doctor.getPerfil()}`)
+          });
 
     }
-    registrarCita(){
+    registrarCita(cita){
+        this.citas.push(cita)
 
     }
-    listarCita(){
+    listarCitas(){
+        this.citas.forEach((cita,y) => {
+            console.log(`(${y+1}) ${cita.getCita()}`)
+        })
 
     }
 }
