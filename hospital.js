61fa2aa5710c9444583dc4bf411ff6ea2f7cd7d5
Juan Ramos <jramos32@ucol.mx>
2020-02-22 21:58:20 -0600

    inicio

---
+++ hospital.js
@@ -0,0 +1,20 @@
+class Hospital{
+    constructor(nombre,direcciones,doctores,citas){
+        this.nombre=nombre;
+        this.direcciones=direcciones;
+        this.doctores=doctores;
+        this.citas=citas;
+    }
+    registrarDoctor(){
+
+    }
+    listarDoctores(){
+
+    }
+    registrarCita(){
+
+    }
+    listarCita(){
+        
+    }
+}
