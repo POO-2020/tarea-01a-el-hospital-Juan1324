4243af13600cabc9494f1693a83b9e52c75b23d7
Juan Ramos <jramos32@ucol.mx>
2020-02-22 21:51:08 -0600

    probarCita

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import Nombre from "./nombre.js"
 import Tiempo from "./tiempo.js"
 import Paciente from "./paciente.js"
 import Doctor from "./doctor.js"
+import Cita from "./cita.js"
 
 class Main{
 
@@ -11,7 +12,7 @@ class Main{
     }
 
     probarFecha(){
-        let años= new Fecha(2020,2,21)
+        let años= new Fecha(2,2,2001)
         console.log(`Años: ${años.getAños()}`);
         console.log(`Meses: ${años.getMeses()}`);
         console.log(`Semana: ${años.getSemanas()}`);
@@ -33,20 +34,29 @@ class Main{
     }
 
     probarPaciente(){
-        let nombre= new Nombre("Rosa ","Campos ","Carrillo")
-        let numerotel=(3121096996)
+        let nombre= new Nombre("Sara ","Campos ","Gómez")
+        let numerotel=("312-109-6996")
         let fecha= new Fecha(2000,2,15)
         let paciente= new Paciente(nombre,numerotel,fecha)
         console.log(paciente.getPerfil())
 
     }
     probarDoctor(){
-        let nombre= new Nombre("Gregorio ","Ramos ","Campos")
-        let especialidad=("Neurologia")
-        let numerotel=(3121099876)
         let cedula=("JKDG12KJDSHJ")
-        let doctor= new Doctor(nombre,especialidad,numerotel,cedula)
+        let especialidad=("Neurologia")
+        let nombre= new Nombre("Gregorio ","Ramos ","Ceballos")
+        let numerotel=("312-109-9876")
+        let doctor= new Doctor(cedula,especialidad,nombre,numerotel)
         console.log(doctor.getPerfil())
+
+    }
+    probarCita(){
+        let fecha = new Fecha(5,10,2002)
+        let hora = new Tiempo(1,44,"pm")
+        let doctor = new Doctor("213131444","Cardiologia",new Nombre("Dr.Cordoba","",""),"JKDG324" )
+        let paciente = new Paciente(new Nombre("Christian ","López ","Astilla"))
+        let cita = new Cita(fecha,hora,doctor,paciente)
+        console.log(cita.getCita())
     }
 
 }
@@ -56,3 +66,4 @@ app.probarNombre();
 app.probarTiempo();
 app.probarPaciente();
 app.probarDoctor();
+app.probarCita();
