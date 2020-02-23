ff11e5460db283d484f3af782667b193c645eb94
Juan Ramos <jramos32@ucol.mx>
2020-02-22 21:51:41 -0600

    getCita

--- cita.js
+++ cita.js
@@ -1,5 +1,16 @@
-class Cita{
-    constructor(){
-        
+import Fecha from "./fecha.js"
+import Nombre from "./nombre.js"
+import Tiempo from "./tiempo.js"
+import Paciente from "./paciente.js"
+import doctor from "./doctor.js"
+export default class Cita{
+    constructor(fecha,hora,doctor,paciente){
+        this.fecha=fecha;
+        this.hora=hora;
+        this.doctor=doctor;
+        this.paciente=paciente;
+    }
+    getCita(){
+        return(`${this.fecha.getFecha()},${this.hora.getFormato24()},${this.doctor.nombre.getNombreCompleto()},${this.paciente.nombre.getNombreCompleto()}`)
     }
 }
