9f31ff03499a959ea50f18354ada4c5b514e84a0
Juan Ramos <jramos32@ucol.mx>
2020-02-22 21:52:00 -0600

    acomodo

--- doctor.js
+++ doctor.js
@@ -1,13 +1,14 @@
 import Nombre from "./nombre.js"
 export default class Doctor{
-    constructor(nombre,especialida,numerotel,cedula){
-        this.nombre=nombre;
-        this.especialida=especialida;
-        this.numerotel=numerotel;
+    constructor(cedula,especialidad,nombre,numerotel){
         this.cedula=cedula;
+        this.especialidad=especialidad;
+        this.nombre=nombre;
+        this.numerotel=numerotel;
+        
 
     }
     getPerfil(){
-        return(`${this.cedula},${this.especialida},${this.nombre.getNombreCompleto()},${this.numerotel}`)
+        return(`${this.cedula},${this.especialidad},${this.nombre.getNombreCompleto()},${this.numerotel}`)
     }
 }
